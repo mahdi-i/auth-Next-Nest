87cbd275348dd5409e69c3cee6415b1ed6c91f59
mahdi-i <mahdi1750idham@gmail.com>
2026-06-20 00:54:42 +0330

    create configuration for main ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { initSwagger } from './common/config/swagger.config';
+import { AllExceptionsFilter } from './common/filter/exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalFilters(new AllExceptionsFilter());
+  initSwagger(app);
+  await app.listen(3001);
 }
 bootstrap();
