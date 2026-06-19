f0d6cbf01a410f6357d7ec5b72338bb22f4408e9
mahdi-i <mahdi1750idham@gmail.com>
2026-06-20 01:03:27 +0330

    refactor connect db

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,11 +3,30 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { typeormAdapter } from '@hedystia/better-auth-typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import dataSource from '@shared/config/dataSource';
 import { betterAuth } from 'better-auth';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres' as const,
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        synchronize: false,
+        entities: ['dist/typeorm/entities/**/*.js', 'dist/**/*.entity.js'],
+        dataSourceFactory: async () => {
+          if (!dataSource.isInitialized) {
+            await dataSource.initialize();
+          }
+          return dataSource;
+        },
+      }),
+    }),
     AuthModule.forRootAsync({
       useFactory: () => ({
         auth: betterAuth({
