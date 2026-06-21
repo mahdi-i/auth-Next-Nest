520b9c35c8f3015332189ef09ce0a223d14d3fc0
mahdi-i <mahdi1750idham@gmail.com>
2026-06-21 13:40:12 +0330

    create confinguration main ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,39 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import dataSource from '@shared/config/data-source';
+import { initSwagger } from '@shared/config/swagger.config';
+import { AllExceptionsFilter } from '@shared/filter/exception.filter';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
-
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidNonWhitelisted: true,
+      whitelist: true,
+      transform: true,
+    }),
+  );
 
-  await app.listen(3001);
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+
+  app.use(cookieParser());
+
+  app.setGlobalPrefix('api');
+
+  await dataSource.initialize();
+
+  if (process.env.NODE_ENV == 'development') initSwagger(app);
+
+  app.useGlobalFilters(new AllExceptionsFilter());
+
+  await app.listen(process.env.PORT ?? 3001, async () => {
+    Logger.log(`Server run on port :  ${process.env.PORT}`, 'AppLogger');
+    Logger.log(`docs on :  ${await app.getUrl()}/docs`, 'AppLogger');
+  });
 }
 bootstrap();
