333752d959156249619afff0c0f60f457380570b
mahdi-i <mahdi1750idham@gmail.com>
2026-06-22 22:50:39 +0330

    create Customization shadcn

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,5 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Roboto, Instrument_Sans } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const instrumentSansHeading = Instrument_Sans({subsets:['latin'],variable:'--font-heading'});
+
+const roboto = Roboto({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -12,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", roboto.variable, instrumentSansHeading.variable)}>
       <body className={`antialiased`}> {children}</body>
     </html>
   );
